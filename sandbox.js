46361a386ffd4d24170036f295168799fa41d452
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-03 01:25:38 +0900

    type basics

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,13 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var character = "mario";
+var age = 30;
+var isBlackBelt = false;
+//character=20;
+character = "luigi";
+//age='yoshi';
+age = 30;
+// isBlackBelt = "yes";
+isBlackBelt = true;
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circle(20));
