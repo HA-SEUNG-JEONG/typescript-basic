3c588aa7a824a229e3d783d71526e656c61b0a6e
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-04 00:16:36 +0900

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("mario");
+let greet;
+// greet = "Hello";
+greet = () => {
+    console.log("hello world");
+};
+//default paramter와 ?은 같이 쓸 수 없음
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(1, 2);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(2, 7);
+// result = "some";
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,23 @@
-console.log("mario");
+let greet: Function;
+
+// greet = "Hello";
+
+greet = () => {
+  console.log("hello world");
+};
+
+//default paramter와 ?은 같이 쓸 수 없음
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(1, 2);
+
+const minus = (a: number, b: number): number => {
+  return a + b;
+};
+
+let result = minus(2, 7);
+
+// result = "some";
