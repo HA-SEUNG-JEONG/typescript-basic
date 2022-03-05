503976b7dc8bf79fc6c9693f08a223684607eab6
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-05 23:37:41 +0900

    private,public and readonly

--- public/app.js
+++ public/app.js
@@ -1,12 +1,16 @@
 "use strict";
 //class
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    //   readonly client: string;
+    //   private details: string; //private은 클래스 외부에서는 접근 불가능
+    //   public amount: number;
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
+        // this.client = "some";
         return "".concat(this.client, " owes \u00A3").concat(this.amount, " for ").concat(this.details);
     };
     return Invoice;
@@ -16,6 +20,10 @@ var invTwo = new Invoice("luigi", "work on  the luigi website", 300);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
+invoices.forEach(function (inv) {
+    //   inv.client = "something"; //readonly : 다른 값으로 할당 불가능
+    console.log(inv.client, inv.amount, inv.format());
+});
 //as를 이용한 type casting
 var form = document.querySelector(".new-item-form");
 // console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 //class
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  //   readonly client: string;
+  //   private details: string; //private은 클래스 외부에서는 접근 불가능
+  //   public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
+    // this.client = "some";
     return `${this.client} owes £${this.amount} for ${this.details}`;
   }
 }
@@ -21,6 +22,11 @@ const invTwo = new Invoice("luigi", "work on  the luigi website", 300);
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  //   inv.client = "something"; //readonly : 다른 값으로 할당 불가능
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 //as를 이용한 type casting
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
